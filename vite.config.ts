066b7a135d3dca4d132206080d0fe6e1f5a24be7
Shihao Cao <shehowcow@gmail.com>
2025-01-26 12:43:42 -0800

    Revert "try tailwind prod but not rlly"
    This reverts commit db6830de6e1fba330ec8355bdd21b78375be6c87.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
-import tailwindcss from '@tailwindcss/vite'
-
+ 
 export default defineConfig({
   server: {
     port: process.env.PORT as unknown as number,
   },
-  plugins: [
-    vercel(),
-    tailwindcss(),
-  ],
+  plugins: [vercel()],
 });
