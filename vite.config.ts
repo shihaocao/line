db6830de6e1fba330ec8355bdd21b78375be6c87
Shihao Cao <shehowcow@gmail.com>
2025-01-26 12:42:08 -0800

    try tailwind prod but not rlly

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
- 
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   server: {
     port: process.env.PORT as unknown as number,
   },
-  plugins: [vercel()],
+  plugins: [
+    vercel(),
+    tailwindcss(),
+  ],
 });
